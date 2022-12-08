53ec7087c84bcff3be0830091e7a4cad756b9d79
sanjaykashyap80 <sanjaykashyap80770@gmail.com>
2022-12-08 14:20:16 +0530

    exercise 3.4 assignment

--- Week-3/exercise3.4.js
+++ Week-3/exercise3.4.js
@@ -1,38 +1,39 @@
-function createStack() { 
-    // let items = [];
-    return { 
-        items:[],
+// function createStack() { 
+//     return { 
+//         items: [],
+//         push(item) {
+//             // console.log("push this----",this)
+//             this.items.push(item);
+//         },
+//         pop() {
+//             // console.log("pop this-----",this)
+//             return this.items.pop();
+//         }
+//         };
+//     }
+//     const stack = createStack();
+//     stack.push(10);
+//     stack.push(5);
+//     stack.pop();// => 5
+//     stack.items;// => [10]
+//     console.log(stack.items)
+//     stack.items = [10,100,1000];// Encapsulation broken!
+
+
+    function createStack() {
+        // Write your code here...
+        let items = []
+        return { 
         push(item) {
-            this.items.push(item);
+            items.push(item);
         },
         pop() {
-            return this.items.pop();
+            return items.pop();
         }
         };
     }
     const stack = createStack();
     stack.push(10);
     stack.push(5);
-    stack.pop();// => 5
-    stack.items;// => [10]
-    console.log(stack.items)
-    stack.items = [10,100,1000];// Encapsulation broken!
-
-
-    // function createStack() {
-    //     // Write your code here...
-    //     return { 
-    //     items:[],
-    //     push(item) {
-    //         this.items.push(item);
-    //     },
-    //     pop() {
-    //         return this.items.pop();
-    //     }
-    //     };
-    // }
-    // const stack = createStack();
-    // stack.push(10);
-    // stack.push(5);
-    // stack.pop(); // => 5
-    // console.log(stack.items);// => undefined
+    stack.pop(); // => 5
+    console.log(stack.items);// => undefined
